9dd9785600850c1568b2ed5a3d28cc5b4b1660b9
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-15 19:11:02 +0900

    dialog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Button from './Components/Button';
+import Dialog from './Components/Dialog';
 
 const AppBlock = styled.div`
   width: 512px;
@@ -23,25 +24,64 @@ const palette = {
 };
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  };
+  const onConfirm = () => {
+    console.log('확인');
+    setDialog(false);
+  };
+  const onCancel = () => {
+    console.log('취소');
+    setDialog(false);
+  };
+
   return (
-    <ThemeProvider theme={{ palette }}>
-      <AppBlock>
-        <ButtonGroup>
-          <Button size='large'>Button</Button>
-          <Button color="gray">Button</Button>
-          <Button size='small' color="pink">Button</Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size='large' outline>Button</Button>
-          <Button color="gray" outline>Button</Button>
-          <Button size='small' color="pink" outline>Button</Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size='large' fullWidth>Button</Button>
-          <Button size='large' color="gray" fullWidth>Button</Button>
-          <Button size='large' color="pink" fullWidth>Button</Button>
-        </ButtonGroup>
-      </AppBlock>
+    <ThemeProvider theme={{ palette }}> {/* ThemProvider는 자식을 한 개 밖에 가질 수 없음. 따라서 fragment 사용 */}
+      <>
+        <AppBlock>
+          <ButtonGroup>
+            <Button size="large">Button</Button>
+            <Button color="gray">Button</Button>
+            <Button size="small" color="pink">
+              Button
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" outline>
+              Button
+            </Button>
+            <Button color="gray" outline>
+              Button
+            </Button>
+            <Button size="small" color="pink" outline>
+              Button
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" fullWidth>
+              Button
+            </Button>
+            <Button size="large" color="gray" fullWidth>
+              Button
+            </Button>
+            <Button size="large" color="pink" fullWidth onClick={onClick}>
+              삭제
+            </Button>
+          </ButtonGroup>
+        </AppBlock>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+          visible={dialog}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 }
